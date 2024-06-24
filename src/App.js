2e7552a5a937f33a3136484f44ff7d4fd1fb526a
guguqueen <guguq59@gmail.com>
2024-06-24 11:32:50 +0200

    added the github repo

--- src/App.js
+++ src/App.js
@@ -3,7 +3,12 @@ import './App.css';
 
 export default function App() {
   return (
-    <div className="App">weather app</div>
+    <div className="App">
+      <h1>weather app</h1>
+    <footer>
+      This project was coded by guguqueen and is {" "}
+    <a href="https://github.com/guguqueen/react-weather-app"> open-sourced on Github </a>
+    </footer>
+    </div>
   );
 }
-
