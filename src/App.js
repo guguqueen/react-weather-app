5e7605ffe9fcc7238cb37d8bc7fef02f054641ca
guguqueen <guguq59@gmail.com>
2024-06-25 10:47:12 +0200

    added bootstrap to my project

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 
+
 import './App.css';
+
 
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
       <h1>weather app</h1>
     <footer>
       This project was coded by guguqueen and is {" "}
     <a href="https://github.com/guguqueen/react-weather-app"> open-sourced on Github </a>
     </footer>
+    </div>
     </div>
   );
 }
