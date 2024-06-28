f9e1b66487431496d557321b6722ddf3837fe936
guguqueen <guguq59@gmail.com>
2024-06-28 12:34:36 +0200

    created a new component and tracked the current unit of the component  to display the relevant temperature

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,47 +1,38 @@
-import React from "react";
-import formattedDate from "./formattedDate";
-import WeatherIcon from "./WeatherIcon.js";
-export default function WeatherInfo(props){
-    return( 
-        <div className="weather">
-    <form>
-    <div className="row">
-        <div className="col- 9">
-    <input type="search" placeholder="enter a city" className="form-control" autoFocus="on"  />
-    </div>
-    <div className="col- 3">
-    <input type="submit" value="search" className="btn btn-primary w-100"/>
-    </div>
-    </div>
-</form>
-<h1>{weatherData.city}</h1>  
-<ul>
-<li> 
-    
-    </li>
-<li className="text-capitalize">{props.Data.description}</li>
-</ul>
 
-<div className="row mt-3">
-    <div className="col- 6">
-        <div className="clearfix " >
-        <div className="float-left">
-        <img src= {weatherData.iconUrl}   />
+import React from "react";
+import FormattedDate from "./FormattedDate";
+import WeatherIcon from "./WeatherIcon";
+import WeatherTemperature from "./WeatherTemperature";
+
+export default function WeatherInfo(props) {
+  return (
+    <div className="WeatherInfo">
+      <h1>{props.data.city}</h1>
+      <ul>
+        <li>
+          <FormattedDate date={props.data.date} />
+        </li>
+        <li className="text-capitalize">{props.data.description}</li>
+      </ul>
+      <div className="row mt-3">
+        <div className="col-6">
+          <div className="d-flex">
+            <div>
+              <WeatherIcon code={props.data.icon} size={52} />
+            </div>
+
+            <div>
+              <WeatherTemperature celsius={props.data.temperature} />
+            </div>
+          </div>
         </div>
-        <div className="float-left">
-       <spam className="temperature"> {Math.round(props.Data.temperature) }</spam>
-       <spam className="unit">degrees</spam>
+        <div className="col-6">
+          <ul>
+            <li>Humidity: {props.data.humidity}%</li>
+            <li>Wind: {props.data.wind} km/h</li>
+          </ul>
+        </div>
+      </div>
     </div>
-    </div>
-    </div>
-    <div className="col- 6">
-        <ul>
-        
-            <li>humudity: {props.Data.humidity} % </li>
-            <li>wind: {props.Data.wind} km/h</li>
-        </ul>
-    </div>
-</div>
-</div>
-);
-} 
+  );
+}
