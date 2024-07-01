7934e4ef95071e4dcb38ab2139659f2c6bd08f6a
guguqueen <guguq59@gmail.com>
2024-07-01 11:04:08 +0200

    .

--- src/weather.js
+++ src/weather.js
@@ -1,5 +1,7 @@
+
 import React, { useState } from "react";
-import WeatherInfo from "./WeatherInfo.js";
+import WeatherInfo from "./WeatherInfo";
+import WeatherForecast from "./WeatherForecast";
 import axios from "axios";
 import "./Weather.css";
 
@@ -31,7 +33,7 @@ export default function Weather(props) {
   }
 
   function search() {
-    const apiKey = "0f8bc384a7c31b717a18cfe38a95ae06";
+        const apiKey = "0f8bc384a7c31b717a18cfe38a95ae06";
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(handleResponse);
   }
